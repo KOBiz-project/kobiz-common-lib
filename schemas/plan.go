e4bac8ba520f39c61d7e3501626eedcf4e6a8f5d
DragonWhite0803 <ehddnr418154@gmail.com>
2025-04-15 05:29:41 +0900

    스키마 수정
    - 요금제: 통신사 + 이름으로 복합 유니크 인덱스 생성

--- schemas/plan.go
+++ schemas/plan.go
@@ -6,8 +6,8 @@ import (
 
 type Plan struct {
 	gorm.Model
-	Telecom    string `gorm:"column:telecom;not null;type:varchar(30);comment:'통신사'" json:"telecom"`
-	NamePlan   string `gorm:"column:name_plan;not null;type:varchar(256);comment:'요금제명'" json:"namePlan"`
+	Telecom    string `gorm:"column:telecom;not null;type:varchar(30);comment:'통신사';uniqueIndex:idx_plan_name" json:"telecom"`
+	NamePlan   string `gorm:"column:name_plan;not null;type:varchar(256);comment:'요금제명';uniqueIndex:idx_plan_name" json:"namePlan"`
 	CodePlan   string `gorm:"column:code_plan;not null;type:varchar(20);comment:'요금제 코드 (Raw Data)'" json:"codePlan"`
 	BasicPrice int64  `gorm:"column:basic_price;not null;type:bigint;comment:'기본요금 (Raw Data)'" json:"basicPrice"`
 	ActiveYn   bool   `gorm:"column:active_yn;default:false" json:"activeYn"`
