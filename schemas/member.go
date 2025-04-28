5d820cf264d1c7aef030e520fe6d6e9ecda2890c
DragonWhite0803 <ehddnr418154@gmail.com>
2025-04-28 15:49:25 +0900

    Feat: 회원 스키마 정의

--- schemas/member.go
+++ schemas/member.go
@@ -6,12 +6,10 @@ import (
 
 type Member struct {
 	gorm.Model
-	Email           string `gorm:"type:varchar(255);unique" json:"email"`
-	Token           string `gorm:"type:varchar(1024)" json:"token"`
-	ProfileImageUrl string `gorm:"type:varchar(512)" json:"profileImageUrl"`
-	SnsType         string `gorm:"type:ENUM('KAKAO','NAVER','FACEBOOK','GOOGLE','APPLE'); DEFAULT:'GOOGLE'" json:"snsType"`
-	ActiveYn        bool   `gorm:"default:false" json:"activeYn"`
-	UserLevel       int8   `gorm:"default:5" json:"userLevel"`
+	Email    string `gorm:"type:varchar(255);comment:'가입 이메일';uniqueIndex:idx_user" json:"email"`
+	SnsType  string `gorm:"type:ENUM('KAKAO','GOOGLE'); default:'GOOGLE';comment:'SNS 로그인 타입';uniqueIndex:idx_user" json:"snsType"`
+	UserType int    `gorm:"column:user_type;default:0;comment:'회원 유형 0: 미선택 1:기업회원 2: 일반회원'" json:"userType"`
+	ActiveYn bool   `gorm:"default:false;comment:'관리자 승인 여부'" json:"activeYn"`
 }
 
 func (Member) TableName() string {
