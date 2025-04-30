ca55758ce5676d04fa6f50baf24741bcc47ad91f
DragonWhite0803 <ehddnr418154@gmail.com>
2025-04-30 16:04:41 +0900

    Fix: 회원 스키마 수정
    - 가입 시 제출서류 필드 추가

--- schemas/member.go
+++ schemas/member.go
@@ -6,10 +6,11 @@ import (
 
 type Member struct {
 	gorm.Model
-	Email    string `gorm:"type:varchar(255);comment:'가입 이메일';uniqueIndex:idx_user" json:"email"`
-	SnsType  string `gorm:"type:ENUM('KAKAO','GOOGLE'); default:'GOOGLE';comment:'SNS 로그인 타입';uniqueIndex:idx_user" json:"snsType"`
-	UserType int    `gorm:"column:user_type;default:0;comment:'회원 유형 0: 미선택 1:기업회원 2: 일반회원'" json:"userType"`
-	ActiveYn bool   `gorm:"default:false;comment:'관리자 승인 여부'" json:"activeYn"`
+	Email        string `gorm:"type:varchar(255);comment:'가입 이메일';uniqueIndex:idx_user" json:"email"`
+	SnsType      string `gorm:"type:ENUM('KAKAO','GOOGLE'); default:'GOOGLE';comment:'SNS 로그인 타입';uniqueIndex:idx_user" json:"snsType"`
+	UserType     int    `gorm:"column:user_type;default:0;comment:'회원 유형 0: 미선택 1:기업회원 2: 일반회원'" json:"userType"`
+	UserDocument string `gorm:"column:user_document;type:varchar(512);comment:'가입 시 제출서류'" json:"userDocument"`
+	ActiveYn     bool   `gorm:"default:false;comment:'관리자 승인 여부'" json:"activeYn"`
 }
 
 func (Member) TableName() string {
