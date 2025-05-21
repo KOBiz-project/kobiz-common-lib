95c860bbff4fb9fa429702c3901dbbf20a056579
DragonWhite0803 <ehddnr418154@gmail.com>
2025-05-21 18:57:01 +0900

    Fix: 스키마 수정
    - 탈퇴 회원 유니크 인덱스 삭제

--- schemas/member-withdrawal.go
+++ schemas/member-withdrawal.go
@@ -7,8 +7,8 @@ import (
 type MemberWithdrawal struct {
 	gorm.Model
 	IdGroup      int    `gorm:"column:id_group;not null;comment:'회원 그룹 아이디'" json:"idGroup"`
-	Email        string `gorm:"column:email;type:varchar(255);comment:'가입 이메일';uniqueIndex:idx_user" json:"email"`
-	SnsType      string `gorm:"column:sns_type;type:ENUM('KAKAO','GOOGLE'); default:'GOOGLE';comment:'SNS 로그인 타입';uniqueIndex:idx_user" json:"snsType"`
+	Email        string `gorm:"column:email;type:varchar(255);comment:'가입 이메일'" json:"email"`
+	SnsType      string `gorm:"column:sns_type;type:ENUM('KAKAO','GOOGLE'); default:'GOOGLE';comment:'SNS 로그인 타입'" json:"snsType"`
 	UserType     int    `gorm:"column:user_type;default:0;comment:'회원 유형 0: 미선택 1:기업회원 2: 일반회원'" json:"userType"`
 	UserDocument string `gorm:"column:user_document;type:varchar(512);comment:'가입 시 제출서류'" json:"userDocument"`
 	Memo1        string `gorm:"column:memo_1;type:varchar(255);comment:'짧은 메모 (회사명)'" json:"memo1"`
