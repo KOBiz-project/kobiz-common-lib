f3c432cf2853f59785c99808a51a5dff6457fbeb
byungjun moon <espeniel69@gmail.com>
2025-05-24 16:49:54 +0900

    aa

--- schemas/log_api.go
+++ schemas/log_api.go
@@ -10,7 +10,7 @@ type LogApi struct {
 	ClientIP               string    `gorm:"column:client_ip;type:varchar(50);comment:'클라이언트 IP 주소'" json:"clientIP"`
 	IDUser                 int       `gorm:"column:id_user;comment:'사용자 ID'" json:"id_user"`
 	ReqBody                string    `gorm:"column:req_body;type:varchar(1000);comment:'요청 본문'" json:"reqBody"`
-	ResResultCode          int       `gorm:"column:res_result_code;comment:'응답 결과 코드'" json:"resResultCode"`
+	ResResultCode          string    `gorm:"column:res_result_code;comment:'응답 결과 코드'" json:"resResultCode"`
 	ResResultDesc          string    `gorm:"column:res_result_desc;type:varchar(255);comment:'응답 결과 설명'" json:"resResultDesc"`
 	ResData                string    `gorm:"column:res_data;type:varchar(9000);comment:'응답 데이터'" json:"resData"`
 	Duration               int64     `gorm:"column:duration;comment:'요청 처리 시간(ms)'" json:"duration"`
