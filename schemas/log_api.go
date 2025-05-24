e2eec061e71a6e5c7b059467496f8a8250f6df64
byungjun moon <espeniel69@gmail.com>
2025-05-24 16:37:56 +0900

    로깅깅

--- schemas/log_api.go
+++ schemas/log_api.go
@@ -14,12 +14,6 @@ type LogApi struct {
 	ResResultDesc          string    `gorm:"column:res_result_desc;type:varchar(255);comment:'응답 결과 설명'" json:"resResultDesc"`
 	ResData                string    `gorm:"column:res_data;type:varchar(9000);comment:'응답 데이터'" json:"resData"`
 	Duration               int64     `gorm:"column:duration;comment:'요청 처리 시간(ms)'" json:"duration"`
-	ReqHeaderAcceptService string    `gorm:"column:req_header_accept_service;type:varchar(255);comment:'Accept-Service 헤더'" json:"reqHeaderAcceptService"`
-	ReqHeaderDeviceInfo    string    `gorm:"column:req_header_device_info;type:varchar(255);comment:'Device-Info 헤더'" json:"reqHeaderDeviceInfo"`
-	ReqHeaderWearableInfo  string    `gorm:"column:req_header_wearable_info;type:varchar(255);comment:'Wearable-Info 헤더'" json:"reqHeaderWearableInfo"`
-	ReqHeaderPackageInfo   string    `gorm:"column:req_header_package_info;type:varchar(255);comment:'Package-Info 헤더'" json:"reqHeaderPackageInfo"`
-	ReqHeaderLocaleInfo    string    `gorm:"column:req_header_locale_info;type:varchar(255);comment:'Locale-Info 헤더'" json:"reqHeaderLocaleInfo"`
-	ReqHeaderUserAgent     string    `gorm:"column:req_header_user_agent;type:varchar(255);comment:'User-Agent 헤더'" json:"reqHeaderUserAgent"`
 	ReqHeaderContentType   string    `gorm:"column:req_header_content_type;type:varchar(255);comment:'Content-Type 헤더'" json:"reqHeaderContentType"`
 	ReqHeaderAuthorization string    `gorm:"column:req_header_authorization;type:varchar(255);comment:'Authorization 헤더'" json:"reqHeaderAuthorization"`
 }
