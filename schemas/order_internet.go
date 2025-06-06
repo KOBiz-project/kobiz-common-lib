b23d7a9d6c0d1b0c2ad06bd3061dcac787afe738
DragonWhite0803 <ehddnr418154@gmail.com>
2025-06-07 05:32:41 +0900

    Feat: 스키마 수정
    - 휴대폰 결합 여부 추가

--- schemas/order_internet.go
+++ schemas/order_internet.go
@@ -11,6 +11,7 @@ type OrderInternet struct {
 	Telecom         string `gorm:"column:telecom;not null;type:varchar(30);comment:'통신사'" json:"telecom"`
 	SpeedType       int    `gorm:"column:speed_type;not null;comment:'인터넷 속도 유형 (1: 100 Mbps, 2: 500 Mbps, 3: 1 Gbps)'" json:"speedType"`
 	TvType          string `gorm:"column:tv_type;not null;type:varchar(30);comment:'TV 유형'" json:"tvType"`
+	CombineYn       bool   `gorm:"column:combine_yn;default:false;comment:'휴대폰 결합 여부'" json:"combineYn"`
 	EstimatedPrice  int64  `gorm:"column:estimated_price;type:bigint;comment:'예상 금액'" json:"estimatedPrice"`
 	Subsidy         int64  `gorm:"column:subsidy;type:bigint;comment:'지원금'" json:"subsidy"`
 }
