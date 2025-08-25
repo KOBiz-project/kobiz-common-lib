d1a80f90586c50f9bda8473ec0923bafc95759e5
byungjun moon <espeniel69@gmail.com>
2025-08-25 14:58:20 +0900

    .

--- dbmanager/admin_member.go
+++ dbmanager/admin_member.go
@@ -9,9 +9,9 @@ type AdminMember struct {
 	Password     string `gorm:"type:varchar(1024);comment:'비밀번호(암호화)'" json:"password"`
 	ActiveYn     bool   `gorm:"default:false;comment:'활성화 여부'" json:"activeYn"`
 	Name         string `gorm:"type:varchar(50);comment:'실명'" json:"name"`
-	NickName     string `gorm:"type:varchar(50);unique;column:nick_name;comment:'닉네임'" json:"nickName"`
-	Email        string `gorm:"type:varchar(255);unique;column:email;comment:'이메일 주소'" json:"email"`
-	Phone        string `gorm:"type:varchar(20);unique;column:phone;comment:'전화번호'" json:"phone"`
+	NickName     string `gorm:"type:varchar(50);column:nick_name;comment:'닉네임'" json:"nickName"`
+	Email        string `gorm:"type:varchar(255);column:email;comment:'이메일 주소'" json:"email"`
+	Phone        string `gorm:"type:varchar(20);column:phone;comment:'전화번호'" json:"phone"`
 	ProfileImage string `gorm:"type:varchar(1024);comment:'프로필 이미지 URL'" json:"profileImage"`
 }
 
