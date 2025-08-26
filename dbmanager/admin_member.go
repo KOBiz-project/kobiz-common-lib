72373a87246c6668618cd9a50f4a9af7e5fdb888
byungjun moon <espeniel69@gmail.com>
2025-08-26 22:28:55 +0900

    .

--- dbmanager/admin_member.go
+++ dbmanager/admin_member.go
@@ -5,7 +5,6 @@ import "gorm.io/gorm"
 type AdminMember struct {
 	gorm.Model
 	UserId       string `gorm:"type:varchar(50);unique;comment:'사용자 ID'" json:"userId"`
-	UserLevel    int8   `gorm:"column:level;not null;default:4;comment:'등급(1:최고관리자,2:일반관리자,3:팀관리자,4:팀원)'" json:"userLevel"`
 	Password     string `gorm:"type:varchar(1024);comment:'비밀번호(암호화)'" json:"password"`
 	ActiveYn     bool   `gorm:"default:false;comment:'활성화 여부'" json:"activeYn"`
 	Name         string `gorm:"type:varchar(50);comment:'실명'" json:"name"`
---
+++ dbmanager/admin_permissions.go
@@ -0,0 +1,81 @@
+package dbmanager
+
+import (
+	"gorm.io/gorm"
+)
+
+// AdminRolePermissions 관리자 역할별 권한 설정 테이블
+type AdminRolePermissions struct {
+	gorm.Model
+	// RoleType 관리자 역할 유형 (1:최고관리자, 2:일반관리자, 3:팀관리자)
+	RoleType int8 `gorm:"column:role_type;not null;comment:'관리자 역할 유형 (1:최고관리자, 2:일반관리자, 3:팀관리자)'" json:"roleType"`
+
+	// 채널 설정 권한들
+	ChannelCreate      bool `gorm:"column:channel_create;default:false;comment:'채널 생성 권한'" json:"channelCreate"`
+	ChannelModify      bool `gorm:"column:channel_modify;default:false;comment:'채널 수정 권한'" json:"channelModify"`
+	ChannelDelete      bool `gorm:"column:channel_delete;default:false;comment:'채널 삭제 권한'" json:"channelDelete"`
+	ChannelGroupManage bool `gorm:"column:channel_group_manage;default:false;comment:'채널 그룹 관리 권한'" json:"channelGroupManage"`
+
+	// DB 관리 권한들
+	DbBackup          bool `gorm:"column:db_backup;default:false;comment:'DB 백업 권한'" json:"dbBackup"`
+	DbDelete          bool `gorm:"column:db_delete;default:false;comment:'DB 삭제 권한'" json:"dbDelete"`
+	DuplicateDbManage bool `gorm:"column:duplicate_db_manage;default:false;comment:'중복 DB 관리 권한'" json:"duplicateDbManage"`
+
+	// 계정 관리 권한들
+	AccountCreate bool `gorm:"column:account_create;default:false;comment:'계정 생성 권한'" json:"accountCreate"`
+	AccountModify bool `gorm:"column:account_modify;default:false;comment:'계정 수정 권한'" json:"accountModify"`
+	AccountDelete bool `gorm:"column:account_delete;default:false;comment:'계정 삭제 권한'" json:"accountDelete"`
+
+	// DB 배정 관리 권한들
+	BackupOrder bool `gorm:"column:backup_order;default:false;comment:'백업 순서 배정 권한'" json:"backupOrder"`
+	TeamBackup  bool `gorm:"column:team_backup;default:false;comment:'팀 백업 권한'" json:"teamBackup"`
+	DataBackup  bool `gorm:"column:data_backup;default:false;comment:'데이터 백업 권한'" json:"dataBackup"`
+
+	// 사이트 설정 권한들
+	AdChannelManage   bool `gorm:"column:ad_channel_manage;default:false;comment:'광고 채널 관리 권한'" json:"adChannelManage"`
+	ApplicationManage bool `gorm:"column:application_manage;default:false;comment:'신청서 관리 권한'" json:"applicationManage"`
+}
+
+// TableName 테이블명 반환
+func (AdminRolePermissions) TableName() string {
+	return "admin_role_permissions"
+}
+
+// UserSpecificPermissions 사용자별 개별 권한 설정 테이블
+type UserSpecificPermissions struct {
+	gorm.Model
+	// AdminMemberID 관리자 회원 ID
+	IDAdminMember uint `gorm:"column:id_admin_member;not null;comment:'관리자 회원 ID'" json:"IdAdminMember"`
+	// RoleType 관리자 역할 유형 (1:최고관리자, 2:일반관리자, 3:팀관리자)
+	RoleType int8 `gorm:"column:role_type;not null;comment:'관리자 역할 유형 (1:최고관리자, 2:일반관리자, 3:팀관리자)'" json:"roleType"`
+
+	// 채널 설정 권한들
+	ChannelCreate      bool `gorm:"column:channel_create;default:false;comment:'채널 생성 권한'" json:"channelCreate"`
+	ChannelModify      bool `gorm:"column:channel_modify;default:false;comment:'채널 수정 권한'" json:"channelModify"`
+	ChannelDelete      bool `gorm:"column:channel_delete;default:false;comment:'채널 삭제 권한'" json:"channelDelete"`
+	ChannelGroupManage bool `gorm:"column:channel_group_manage;default:false;comment:'채널 그룹 관리 권한'" json:"channelGroupManage"`
+
+	// DB 관리 권한들
+	DbBackup          bool `gorm:"column:db_backup;default:false;comment:'DB 백업 권한'" json:"dbBackup"`
+	DbDelete          bool `gorm:"column:db_delete;default:false;comment:'DB 삭제 권한'" json:"dbDelete"`
+	DuplicateDbManage bool `gorm:"column:duplicate_db_manage;default:false;comment:'중복 DB 관리 권한'" json:"duplicateDbManage"`
+
+	// 계정 관리 권한들
+	AccountCreate bool `gorm:"column:account_create;default:false;comment:'계정 생성 권한'" json:"accountCreate"`
+	AccountModify bool `gorm:"column:account_modify;default:false;comment:'계정 수정 권한'" json:"accountModify"`
+	AccountDelete bool `gorm:"column:account_delete;default:false;comment:'계정 삭제 권한'" json:"accountDelete"`
+
+	// DB 배정 관리 권한들
+	BackupOrder bool `gorm:"column:backup_order;default:false;comment:'백업 순서 배정 권한'" json:"backupOrder"`
+	TeamBackup  bool `gorm:"column:team_backup;default:false;comment:'팀 백업 권한'" json:"teamBackup"`
+	DataBackup  bool `gorm:"column:data_backup;default:false;comment:'데이터 백업 권한'" json:"dataBackup"`
+
+	// 사이트 설정 권한들
+	AdChannelManage   bool `gorm:"column:ad_channel_manage;default:false;comment:'광고 채널 관리 권한'" json:"adChannelManage"`
+	ApplicationManage bool `gorm:"column:application_manage;default:false;comment:'신청서 관리 권한'" json:"applicationManage"`
+}
+
+// TableName 테이블명 반환
+func (UserSpecificPermissions) TableName() string {
+	return "user_specific_permissions"
+}
