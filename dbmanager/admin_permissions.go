76c9f3a0c559ee309ec7facc001f4dd73952338f
Moon <espeniel69@gmail.com>
2025-09-09 21:32:35 +0900

    추가: 레벨별 권한 및 매핑 테이블 구조체 정의

--- dbmanager/admin_permissions.go
+++ dbmanager/admin_permissions.go
@@ -111,15 +111,29 @@ func (MenuPermission) TableName() string {
 	return "menu_permissions"
 }
 
-// UserMenuPermission 사용자 메뉴 권한 테이블
-type UserMenuPermission struct {
+// LevelPermission 레벨별 권한 테이블
+type LevelPermission struct {
 	gorm.Model
-	ID               uint `gorm:"column:id;primaryKey" json:"id" comment:"사용자 권한 인덱스"`
-	IDMenuPermission uint `gorm:"column:id_munu_permission;not null" json:"idMenuPermission" comment:"메뉴 인덱스"`
-	IDAdminMember    uint `gorm:"column:id_admin_member;not null" json:"idAdminMember" comment:"관리자 인덱스"`
+	Name     string `gorm:"column:name;type:varchar(100);not null" json:"name" comment:"레벨별 권한 명"`
+	Desc     string `gorm:"column:desc;type:varchar(255);not null" json:"desc" comment:"설명"`
+	ActiveYn bool   `gorm:"column:active_yn;type:boolean;default:false;not null" json:"activeYn" comment:"활성여부"`
+	RoleType int8   `gorm:"column:role_type;type:tinyint;default:1;not null" json:"roleType" comment:"관리자 역할 유형 (1~10 범위)"`
 }
 
 // TableName 테이블명 반환
-func (UserMenuPermission) TableName() string {
-	return "user_menu_permissions"
+func (LevelPermission) TableName() string {
+	return "level_permissions"
+}
+
+// LevelPermissionsMenuPermissions 레벨별 권한과 메뉴 권한 매핑 테이블
+type LevelPermissionsMenuPermissions struct {
+	gorm.Model
+	IDLevelPermission uint `gorm:"column:id_level_permission;not null" json:"idLevelPermission" comment:"레벨별 권한 인덱스"`
+	IDMenuPermission  uint `gorm:"column:id_menu_permission;not null" json:"idMenuPermission" comment:"메뉴 권한 인덱스"`
+	ActiveYn          bool `gorm:"column:active_yn;type:boolean;default:false;not null" json:"activeYn" comment:"활성여부"`
+}
+
+// TableName 테이블명 반환
+func (LevelPermissionsMenuPermissions) TableName() string {
+	return "level_permissions_menu_permissions"
 }
