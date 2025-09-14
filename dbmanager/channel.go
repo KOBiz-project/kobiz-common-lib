eaf392d5119606ea2b8ed98836699cee3f6612df
Moon <espeniel69@gmail.com>
2025-09-14 23:12:39 +0900

    추가: Channel 구조체에 메인이미지 주소, 서브이미지 주소 및 버튼 문구 필드 추가

--- dbmanager/channel.go
+++ dbmanager/channel.go
@@ -12,6 +12,9 @@ type Channel struct {
 	IdAdminTeam      uint      `gorm:"column:id_admin_team;not null;type:int;comment:'배정 관리자 팀 ID'" json:"idAdminTeam"`
 	AssignmentMethod int8      `gorm:"column:assignment_method;not null;type:tinyint;default:0;comment:'배정 방법(0: 자동 순차, 1: 수동, 2: 팀 배정)'" json:"assignmentMethod"`
 	RepeatDbCheckYn  bool      `gorm:"column:repeat_db_check_yn;not null;type:boolean;default:false;comment:'중복 DB 체크 여부'" json:"repeatDbCheckYn"`
+	MainImageUrl     string    `gorm:"column:main_image_url;type:varchar(255);comment:'메인이미지 주소'" json:"mainImageUrl"`
+	SubImageUrl      string    `gorm:"column:sub_image_url;type:varchar(255);comment:'서브이미지 주소'" json:"subImageUrl"`
+	ButtonText       string    `gorm:"column:button_text;type:varchar(255);comment:'버튼 문구'" json:"buttonText"`
 	ActiveYn         bool      `gorm:"column:active_yn;not null;type:boolean;default:true;comment:'활성 여부'" json:"activeYn"`
 	CreatedAt        time.Time `gorm:"column:created_at;not null;type:datetime;comment:'생성일';default:CURRENT_TIMESTAMP" json:"createdAt"`
 	UpdatedAt        time.Time `gorm:"column:updated_at;not null;type:datetime;comment:'수정일';default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP" json:"updatedAt"`
@@ -40,9 +43,6 @@ type ChannelChannelGroup struct {
 	ID             uint      `gorm:"primaryKey;autoIncrement" json:"id"`
 	ChannelID      uint      `gorm:"column:channel_id;not null;type:int;comment:'채널 ID';index" json:"channelId"`
 	ChannelGroupID uint      `gorm:"column:channel_group_id;not null;type:int;comment:'그룹 ID';index" json:"channelGroupId"`
-	MainImageUrl   string    `gorm:"column:main_image_url;type:varchar(255);comment:'메인이미지 주소'" json:"mainImageUrl"`
-	SubImageUrl    string    `gorm:"column:sub_image_url;type:varchar(255);comment:'서브이미지 주소'" json:"subImageUrl"`
-	ButtonText     string    `gorm:"column:button_text;type:varchar(255);comment:'버튼 문구'" json:"buttonText"`
 	CreatedAt      time.Time `gorm:"column:created_at;not null;type:datetime;comment:'생성일';default:CURRENT_TIMESTAMP" json:"createdAt"`
 	UpdatedAt      time.Time `gorm:"column:updated_at;not null;type:datetime;comment:'수정일';default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP" json:"updatedAt"`
 }
