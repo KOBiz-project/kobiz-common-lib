5d612a05a388a201248d607a431d9157764b43c0
Moon <espeniel69@gmail.com>
2025-09-15 22:24:19 +0900

    추가: Channel 구조체에 채널 코드 필드 추가

--- dbmanager/channel.go
+++ dbmanager/channel.go
@@ -8,6 +8,7 @@ import (
 type Channel struct {
 	ID               uint      `gorm:"primaryKey;autoIncrement" json:"id"`
 	Name             string    `gorm:"column:name;not null;type:varchar(100);comment:'채널명';uniqueIndex:idx_channel_name" json:"name"`
+	Code             string    `gorm:"column:code;not null;type:varchar(100);comment:'채널 코드';uniqueIndex:idx_channel_code" json:"code"`
 	Description      string    `gorm:"column:description;type:varchar(255);comment:'채널 설명'" json:"description"`
 	IdAdminTeam      uint      `gorm:"column:id_admin_team;not null;type:int;comment:'배정 관리자 팀 ID'" json:"idAdminTeam"`
 	AssignmentMethod int8      `gorm:"column:assignment_method;not null;type:tinyint;default:0;comment:'배정 방법(0: 자동 순차, 1: 수동, 2: 팀 배정)'" json:"assignmentMethod"`
