162344080c3b516ab1f3a66ed0d8b340d3a5b8d4
Moon <espeniel69@gmail.com>
2025-09-21 01:11:21 +0900

    Refactor: Reorganize fields in AdminMember struct for improved readability and maintainability

--- dbmanager/admin_member.go
+++ dbmanager/admin_member.go
@@ -8,15 +8,16 @@ import (
 
 type AdminMember struct {
 	gorm.Model
-	UserId       string `gorm:"type:varchar(50);unique;comment:'사용자 ID'" json:"userId"`
-	Password     string `gorm:"type:varchar(1024);comment:'비밀번호(암호화)'" json:"password"`
-	ActiveYn     bool   `gorm:"default:false;comment:'활성화 여부'" json:"activeYn"`
-	Name         string `gorm:"type:varchar(50);comment:'실명'" json:"name"`
-	NickName     string `gorm:"type:varchar(50);unique;column:nick_name;comment:'닉네임'" json:"nickName"`
-	Email        string `gorm:"type:varchar(255);unique;column:email;comment:'이메일 주소'" json:"email"`
-	Phone        string `gorm:"type:varchar(20);unique;column:phone;comment:'전화번호'" json:"phone"`
-	ProfileImage string `gorm:"type:varchar(1024);comment:'프로필 이미지 URL'" json:"profileImage"`
-	IdAdminTeam  uint   `gorm:"column:id_admin_team;comment:'관리자 팀 번호'" json:"adminTeam"`
+	UserId            string `gorm:"type:varchar(50);unique;comment:'사용자 ID'" json:"userId"`
+	Password          string `gorm:"type:varchar(1024);comment:'비밀번호(암호화)'" json:"password"`
+	ActiveYn          bool   `gorm:"default:false;comment:'활성화 여부'" json:"activeYn"`
+	Name              string `gorm:"type:varchar(50);comment:'실명'" json:"name"`
+	NickName          string `gorm:"type:varchar(50);unique;column:nick_name;comment:'닉네임'" json:"nickName"`
+	Email             string `gorm:"type:varchar(255);unique;column:email;comment:'이메일 주소'" json:"email"`
+	Phone             string `gorm:"type:varchar(20);unique;column:phone;comment:'전화번호'" json:"phone"`
+	ProfileImage      string `gorm:"type:varchar(1024);comment:'프로필 이미지 URL'" json:"profileImage"`
+	IdAdminTeam       uint   `gorm:"column:id_admin_team;comment:'관리자 팀 번호'" json:"adminTeam"`
+	IdLevelPermission uint   `gorm:"column:id_level_permission;comment:'레벨별 권한 번호'" json:"idLevelPermission"`
 }
 
 func (AdminMember) TableName() string {
